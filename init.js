0ad00c72e82df42f326468a8f7eef695974725c5
asafonov <me@asafonov.org>
2024-03-06 15:33:34 +0000

    init fix

--- init.js
+++ init.js
@@ -1,6 +1,5 @@
 document.addEventListener("DOMContentLoaded", function (event) {
   const updaterView = new UpdaterView('https://raw.githubusercontent.com/asafonov/accelerace/master/VERSION.txt', 'https://github.com/asafonov/accelerace.apk/releases/download/{VERSION}/app-release.apk')
   updaterView.showUpdateDialogIfNeeded()
-  const roadLinesView = new RoadLinesView(6)
-  const decorView = new DecorView(32, 2)
+  const roadView = new RoadView(32, 2)
 })
