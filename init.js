ae3e4ac9ac77582d9553270670822f2f2a8d2fb5
asafonov <me@asafonov.org>
2024-03-18 15:46:49 +0000

    minor fix

--- init.js
+++ init.js
@@ -1,6 +1,5 @@
 document.addEventListener("DOMContentLoaded", function (event) {
   const updaterView = new UpdaterView('https://raw.githubusercontent.com/asafonov/accelerace/master/VERSION.txt', 'https://github.com/asafonov/accelerace.apk/releases/download/{VERSION}/app-release.apk')
   updaterView.showUpdateDialogIfNeeded()
-  asafonov.timer.setInterval(100)
   const gameView = new GameView()
 })
