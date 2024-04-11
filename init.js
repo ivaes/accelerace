b8d5e2bc756a8c068435f426e0d484928f7ac2c1
asafonov <me@asafonov.org>
2024-04-11 11:57:31 +0000

    disable in-app update

--- init.js
+++ init.js
@@ -1,6 +1,4 @@
 document.addEventListener("DOMContentLoaded", function (event) {
-  const updaterView = new UpdaterView('https://raw.githubusercontent.com/asafonov/accelerace/master/VERSION.txt', 'https://github.com/asafonov/accelerace.apk/releases/download/{VERSION}/app-release.apk')
-  updaterView.showUpdateDialogIfNeeded()
   asafonov.timer.setInterval(80)
   const gameView = new GameView(5500)
 })
